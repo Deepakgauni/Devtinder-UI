3127127d83b478dc9bb5948647a5b93206a071ba
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2025-09-21 01:49:04 +0530

    Updated feature

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
      <div className=" flex justify-center my-10">
     <div className="card bg-base-300 w-96 shadow-sm">
   <div className="card-body">
-    <h2 className="card-title">Login</h2>
+    <h2 className="card-title">Login Now</h2>
     
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Buy Now</button>

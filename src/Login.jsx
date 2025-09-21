bf843c3a08615f09110943cde5835b82d4c56871
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2025-09-21 16:58:55 +0530

    adding login or password

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-
+ import React from 'react'
+ import { useState} from 'react'
 const Login = () => {
+  const[emailId, setEmailId]= useState("");
+  const[password, setPassword]= useState("");
   return (
      <div className=" flex justify-center my-10">
     <div className="card bg-base-300 w-96 shadow-sm">
   <div className="card-body">
-    <h2 className="card-title">Login Now</h2>
+    <h2 className="card-title justify-center">Login Now</h2>
+    <div>
+    <fieldset className="fieldset">
+   <legend className="fieldset-legend">Email ID</legend>
+   <input type="text" value={emailId} className="input" placeholder="Type here"
+   onChange={(e)=>setEmailId(e.target.value)} />
+   
+   </fieldset>
+       <fieldset className="fieldset">
+   <legend className="fieldset-legend">Password</legend>
+   <input type="text" value={password} className="input" placeholder="Type here"
+    onChange={(e)=>setPassword(e.target.value)} />
+   
+   </fieldset>
+
+</div>
     
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <div className="card-actions justify-center">
+      <button className="btn btn-primary">Login</button>
     </div>
   </div>
 </div>
